5d8b200e08e27cbcc7750f056026d3bb93acf3c2
Chanthai1102 <114852768+Chanthai1102@users.noreply.github.com>
2023-12-02 14:48:40 +0700

    Update J024ForthPattern.java

--- src/J024ForthPattern.java
+++ src/J024ForthPattern.java
@@ -1,6 +1,7 @@
 package src;
 
 public class J024ForthPattern {
+<<<<<<< Updated upstream
     public static void main(String args[]) {
         int x, y;
         for (x = 1; x <= 7; x++) {
@@ -9,5 +10,15 @@ public class J024ForthPattern {
             }
             System.out.println();
         }
+=======
+    public static void main(String args[]){
+         int x,y;
+         for(x=1;x<=7;x++) {
+            for(y=1;y<x;y++) {
+                System.out.print("*");
+                }
+            System.out.println();
+            }
+>>>>>>> Stashed changes
     }
 }
