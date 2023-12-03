46e337b230672abbf5c5da344356e4bfcb309326
Chanthai1102 <114852768+Chanthai1102@users.noreply.github.com>
2023-12-03 16:02:58 +0700

    update file today

---
+++ src/J067DecToBin.java
@@ -0,0 +1,25 @@
+package src;
+
+public class J067DecToBin {
+    public void Convert(int num1) {
+        int bin[] = new int[40];
+        int ind = 0;
+
+        while(num1 >0) {
+            bin[ind++] = num1%2;
+            num1 = num1/2;
+        }
+        for(int x=ind-1; x>=0; x--) {
+            System.out.print(bin[x]);
+        }
+    }
+        public static void main(String args[]) {
+        J067DecToBin dtb = new J067DecToBin();
+        System.out.println("Binary equivalent of 88 is: ");
+        dtb.Convert(88);
+        System.out.println("\n Binary equivalent of 55 is: ");
+        dtb.Convert(55);
+        System.out.println("\n Binary equivalent of 10 is: ");
+        dtb.Convert(10);
+        }
+}
